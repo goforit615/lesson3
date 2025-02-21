d1173432a2cea08bfee09829594f4160a47a3fef
goforit615 <bupark82@gsm.hs.kr>
2025-02-21 10:58:54 +0900

    OperationEX5

---
+++ src/operator/OperationEx5.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperationEx5 {
+    public static void main(String[] args) {
+        int num = 0B00000101;
+        System.out.println(num << 2);   // 왼쪽으로 2비트 이동 00010100 (20)
+        System.out.println(num >> 2);   // 오른쪽으로 2비트 이동 00000001 (1)
+        System.out.println(num >>> 2);  // 오른쪽으로 2비트 이동 00000001 (1)
+
+        System.out.println(num);
+
+        num = num << 2;
+        System.out.println(num);
+    }
+}
+
